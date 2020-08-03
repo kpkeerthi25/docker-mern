920668e709c682e7cbe9fb3b717f0923104d0119
kpkeerthi <kpkeerthi34@gmail.com>
2020-08-03 23:29:02 +0530

    docker-compose-prod

--- client/src/App.js
+++ client/src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React keerthi vasan
         </a>
       </header>
     </div>
