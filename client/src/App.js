c78587a7627ce7b8cf1cf4f332bce4851a90437c
kpkeerthi <kpkeerthi34@gmail.com>
2020-08-04 19:25:28 +0530

    ci/cd final test

--- client/src/App.js
+++ client/src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React keerthi vasan
+          Learn React keerthi vasan last test
         </a>
       </header>
     </div>
